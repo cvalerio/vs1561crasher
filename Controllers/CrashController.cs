d613c15d23d8fa61ff8e6fc649857012f7ad4728
Claudio Valerio <claudio.valerio@zero.it>
2018-03-09 17:06:31 +0100

    'Init'

---
+++ Controllers/CrashController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VS1561Crasher.Controllers
+{
+   [Route("api/[controller]")]
+   public class CrashController : Controller
+   {
+      private readonly IDependency _dependency;
+
+      public CrashController(IDependency dependency)
+      {
+         _dependency = dependency;
+      }
+
+      [HttpPost]
+      public string DoCrash()
+      {
+         return "Isn't crashed";
+      }
+   }
+
+   public interface IDependency
+   {
+   }
+}
---
+++ Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using VS1561Crasher.Controllers;
+
+namespace VS1561Crasher
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+           services.AddTransient<IDependency, ConcreteDependency>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+
+   public class ConcreteDependency : IDependency
+   {
+   }
+}
